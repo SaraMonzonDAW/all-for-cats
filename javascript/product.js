1edd4da1e6aed07823da95543eb7bfc286e43a00
Sara Monzón Quesada <saramonzonquesada@alumno.ieselrincon.es>
2025-01-14 17:06:42 +0000

    feat(all-for-cats): product page card update

--- javascript/product.js
+++ javascript/product.js
@@ -25,14 +25,16 @@ document.addEventListener("DOMContentLoaded", function () {
           contentList += `
                         <li class="list__card">
                             <img src="${producto.imagen}" alt="${producto.nombre}" class="list__card-img">
-                            <p>${producto.nombre}</p>
-                            <p>${producto.precioOriginal}€</p>
+                            <p class="list__card-name">${producto.nombre}</p>
+                            <p class="list__card-price">${producto.precioOriginal}€</p>
+                           <div class="list__card-actions">
                             <div class="list__card-counter">
                                 <button class="list_card-counterMinus"><svg width="65px" height="65px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12L18 12" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
                                 <span class="list_card-counterText">0</span>
                                 <button class="list_card-counterPlus"><svg width="65px" height="65px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M12 4V20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
                             </div>
-                            <button>Añadir</button>
+                            <button class="list__card-add">Añadir</button>
+                            </div>
                         </li>
                     `;
         });
